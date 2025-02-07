1824167b532ef370d239d76955a0984c50b90ca8
leytonkm <leytonm2@illinois.edu>
2025-02-07 15:28:20 -0600

    created string

--- typingspeed.py
+++ typingspeed.py
@@ -1,2 +1,13 @@
-print('typingspeedtest')
+import time
+import random
+import json
 
+with open('typing_test_words_1000.json', 'r') as word_doc:
+    data = json.load(word_doc)  # Load JSON data correctly
+
+complete_list = data['typing_test_words']
+
+random_50 = random.sample(complete_list, 50)
+
+typing_test_string = ' '.join(random_50)
+print(typing_test_string)
